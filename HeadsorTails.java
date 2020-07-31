6491b3d0150ca65001fc95cb1850fd61e2e32467
bunooboi <bunooboi@gmail.com>
2020-07-31 14:46:01 +0900

    HoT: Initial revision

---
+++ HeadsorTails.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class HeadsorTails{
+    public static void main(String args[]) throws IOException{
+	BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+
+	int round[]=new int[3];
+	
+	round[0]=(int)(Math.random()*2);
+	round[1]=(int)(Math.random()*2);
+	round[2]=(int)(Math.random()*2);
+
+	System.out.println("Tossing a coin...");
+
+	int head=0;
+	int tail=0;
+	for(int i=0;i<3;i++){
+	    if(round[i]==0){
+		head++;
+		System.out.println("Round "+(i+1)+": Heads");
+	    }else{
+		tail++;
+		System.out.println("Round "+(i+1)+": Tails");		
+	    }
+	}
+
+	System.out.println("Heads: "+head+", Tails: "+tail);
+    }
+}
